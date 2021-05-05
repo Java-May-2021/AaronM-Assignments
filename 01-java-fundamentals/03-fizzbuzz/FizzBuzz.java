fa84414fe0b68cf4f360fd5c2477180bef4b4012
unknown <aaronsmorgan88@gmail.com>
2021-05-05 16:00:15 -0700

    FizzBuzz added

---
+++ 01-java-fundamentals/03-fizzbuzz/FizzBuzz.java
@@ -0,0 +1,23 @@
+public class FizzBuzz {
+    public String fizzBuzz (int value){
+        if (value %15 == 0){
+            return "FizzBuzz";
+        } 
+        else if (value % 3 == 0){
+            return "Fizz";
+        }
+        else if (value % 5==0){
+            return "Buzz";
+        }
+        else {
+            return Integer.toString(value);
+        }
+    }
+
+    public void counter() {
+        for (int i = 1; i <= 100; i++){
+            String result = fizzBuzz(i);
+            System.out.println("Number:" + i + " - Results: " +  result);
+        }
+    }
+}
