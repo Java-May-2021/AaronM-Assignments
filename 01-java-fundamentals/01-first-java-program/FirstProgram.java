1aa981ffbb47d497f5d35852f0d47e23451677ae
unknown <aaronsmorgan88@gmail.com>
2021-05-05 12:01:06 -0700

    first program added

---
+++ 01-java-fundamentals/01-first-java-program/FirstProgram.java
@@ -0,0 +1,7 @@
+public class FirstProgram {
+    public static void main(String[] args){
+        System.out.println("My name is Aaron Morgan");
+        System.out.println("I am 32 years old");
+        System.out.println("My hometown is Seattle, WA");
+    }
+}
